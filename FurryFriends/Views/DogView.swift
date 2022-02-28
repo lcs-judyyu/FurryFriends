226740e8ef06746fed16229a4484ace582b65241
Judy <jyu@lcs.on.ca>
2022-02-28 09:40:10 -0500

    Add a border for the image

--- FurryFriends/Views/DogView.swift
+++ FurryFriends/Views/DogView.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+//button style
+struct GrowingButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.vertical, 10)
+            .padding(.horizontal, 15)
+            .background(configuration.isPressed ? Color.orange.opacity(0.5) : Color.orange.opacity(0.2))
+            .foregroundColor(.black)
+            .clipShape(RoundedRectangle(cornerRadius: 5, style: .continuous))
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.orange, lineWidth: 2)
+            )
+            .scaleEffect(configuration.isPressed ? 1.06 : 1)
+            .animation(.easeOut(duration: 0.3), value: configuration.isPressed)
+    }
+}
+
 struct DogView: View {
     
     // MARK: Stored properties
@@ -19,13 +37,18 @@ struct DogView: View {
     var body: some View {
         
         ZStack {
-            Color.yellow.opacity(0.4)
+            Color.yellow.opacity(0.3)
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
                 
                 // Shows the main image
                 RemoteImageView(fromURL: currentImage)
+                    .padding(15)
+                    .border(Color.gray, width: 4)
+                    .padding()
+                
+                //Button for regenerating an image
                 
                 // Push main image to top of screen
                 Spacer()
@@ -35,7 +58,7 @@ struct DogView: View {
             .task {
                 
                 // Example images for each type of pet
-                let remoteCatImage = "https://purr.objects-us-east-1.dream.io/i/JJiYI.jpg"
+                //let remoteCatImage = "https://purr.objects-us-east-1.dream.io/i/JJiYI.jpg"
                 let remoteDogImage = "https://images.dog.ceo/breeds/labrador/lab_young.JPG"
                 
                 // Replaces the transparent pixel image with an actual image of an animal
