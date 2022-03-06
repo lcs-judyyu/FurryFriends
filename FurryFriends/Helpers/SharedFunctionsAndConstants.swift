3ba447cafd2df883291072763cc1c73fb064324e
Judy <jyu@lcs.on.ca>
2022-03-05 22:39:30 -0500

    Create a file to return the location of the Documents directory and define file name

---
+++ FurryFriends/Helpers/SharedFunctionsAndConstants.swift
@@ -0,0 +1,22 @@
+//
+//  SharedFunctionsAndConstants.swift
+//  FurryFriends
+//
+//  Created by Judy Yu on 2022-03-05.
+//
+
+import Foundation
+
+//return the location of the Documents directory for the app
+func getDocumentsDirectoryForDogs() -> URL{
+    let paths = FileManager.default.urls(for: .documentDirectory,
+                                            in: .userDomainMask)
+    
+    //return the first path
+    return paths[0]
+}
+
+//define filename that we will write data to in the directory
+let savedFavouriteDogsLabel = "savedFavouriteDogs"
+
+let savedFavouriteCatsLabel = "savedFavouriteCats"
